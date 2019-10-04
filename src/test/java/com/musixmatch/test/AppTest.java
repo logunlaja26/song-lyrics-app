45dda0c7e813b42fa124224c38b6b1d2bfdbcfa3
Ly Ogunlaja <logunlaja26@gmail.com>
2019-10-04 17:18:48 -0500

    Modified mappings

--- src/test/java/com/musixmatch/test/AppTest.java
+++ src/test/java/com/musixmatch/test/AppTest.java
@@ -1,10 +1,13 @@
 package com.musixmatch.test;
 
 import org.junit.Test;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 public class AppTest {
 	@Test
+	@GetMapping("/home")
 	public void test() {
 		String uri = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=ea763ea7d9e5f955b69ed38ea84ba970&track_id=15953433";
 		RestTemplate restTemplate = new RestTemplate();
